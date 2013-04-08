5f9c552780b2fae13a171894c21956d1a3443d61
Kim Crowder <speshalkim@gmail.com>
2013-04-08 15:01:56 -0500

    Second Ruby Test Edit2

--- array.rb
+++ array.rb
@@ -1,3 +1,6 @@
 # loop through array
 # display each item in array
-[["Greg", "Jim", "Kim"]].each { |m| puts m }
+#[["Greg", "Jim", "Kim"]].each { |m| puts m }
+
+# display only "Jim" from array [["Greg", "Jim", "Kim"]]
+puts [["Greg", "Jim", "Kim"]][0][1]
